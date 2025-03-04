6531b05ed59637b7cb9205d6acd3ef7a7936fdb1
lucasptcastro <lucasptcastro@gmail.com>
2025-03-04 12:59:18 -0300

    ✨ feat: added formatCurrency function

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
+
 interface ProductProps {
   products: Product[];
 }
@@ -26,10 +28,7 @@ const Products = ({ products }: ProductProps) => {
             </p>
 
             <p className="pt-3 text-sm font-semibold">
-              {Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL",
-              }).format(product.price)}
+              {formatCurrency(product.price)}
             </p>
           </div>
 
