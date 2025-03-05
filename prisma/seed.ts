25224346488d5c06ea30e8c72b4bcb5ba3d4fb1f
lucasptcastro <lucasptcastro@gmail.com>
2025-03-05 13:11:54 -0300

    🦄 refactor: removed comments

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 const { PrismaClient } = require("@prisma/client");
 
 const prismaClient = new PrismaClient();
